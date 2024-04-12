89de7960b7b1e50f43bc70b19be3eef066ace8e3
wwwwwmw <william.manwai@gmail.com>
2024-04-12 16:19:33 +0100

    Update Navbar.jsx, PromptInput.jsx, and Sidebar.jsx components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,48 @@
-// src/components/Navbar.js
+// src/components/Navbar.jsx
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShareNodes } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
-    <nav className="bg-gray-100">
-      {/* ... Your navbar content */}
-      <span className="text-xl">Navbar</span>
-    </nav>
+    <div className="sticky top-0 z-50 bg-white text-white p-4 flex justify-between items-center">
+      {/* Left side - Dropdown menu */}
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant="light">Open Menu</Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Static Actions">
+          <DropdownItem key="new" shortcut="⌘N">
+            Professional
+          </DropdownItem>
+          <DropdownItem key="copy" shortcut="⌘C">
+            Epic
+          </DropdownItem>
+          <DropdownItem key="edit" shortcut="⌘E">
+            Legendary
+          </DropdownItem>
+          <DropdownItem
+            key="delete"
+            shortcut="⌘D"
+            className="text-danger"
+            color="danger"
+          >
+            Delete file
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+
+      {/* Right side - Share button */}
+
+      <Button auto flat bordered color="light" variant="faded">
+        <FontAwesomeIcon icon={faShareNodes} className="text-gray-800" />
+      </Button>
+    </div>
   );
 };
 

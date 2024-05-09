9274a6a596d7387a713af3cb54824f863d2e14c0
wwwwwmw <william.manwai@gmail.com>
2024-05-09 23:34:23 +0100

    Refactor dependencies, update PropTypes, and remove unused code

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 // src/components/Sidebar.jsx
+import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";

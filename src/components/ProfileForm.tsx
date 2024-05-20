a946b10e19053459cf106dda79c922cccd740aba
wwwwwmw <william.manwai@gmail.com>
2024-05-20 15:00:38 +0100

    chore: Update .env file with Firebase configuration

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { doc, setDoc } from "firebase/firestore";
-import { db } from "../firebaseConfig";
+import { auth, db, provider } from '../firebaseConfig';
 import { useNavigate } from "react-router-dom";
 
 const ProfileForm: React.FC = () => {
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const apiKey = import.meta.env.VITE_APP_FIREBASE_API_KEY;
+const authDomain = import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_APP_FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_APP_FIREBASE_APP_ID;
+const measurementId = import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID;
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: `${apiKey}`,
+  authDomain: `${authDomain}`,
+  projectId: `${projectId}`,
+  storageBucket: `${storageBucket}`,
+  messagingSenderId: `${messagingSenderId}`,
+  appId: `${appId}`,
+  measurementId: `${measurementId}`
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
